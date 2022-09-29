206f9879b6c6c78eb586ebe4568956a3f48d3f90
Kristian Matthews <kristian.matthews@me.com>
2022-09-29 11:00:01 +0100

    Fix

--- migrations/20220928194430-add-order-model.js
+++ migrations/20220928194430-add-order-model.js
@@ -2,20 +2,53 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      domain: {
+        type: Sequelize.STRING,
+      },
+      orderId: {
+        unique: true,
+        type: Sequelize.BIGINT,
+      },
+      orderStatusUrl: {
+        type: Sequelize.STRING,
+      },
+      lineItems: {
+        type: Sequelize.JSON,
+      },
+      fulfillmentStatus: {
+        type: Sequelize.STRING,
+      },
+      fulfillmentId: {
+        type: Sequelize.BIGINT,
+      },
+      fulfillmentDate: {
+        type: Sequelize.DATE,
+      },
+      fulfillmentTrackingCompany: {
+        type: Sequelize.STRING,
+      },
+      fulfillmentTrackingNumber: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Orders');
+  },
 };
